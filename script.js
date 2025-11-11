37d1c7482af288f3b7478a603702023f6c3e4a57
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-11-11 10:26:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-//your code here
+let input = document.getElementById("input");
+
+for (let i = 0; i <= 9; i++) {
+  document.getElementById("block" + i).addEventListener("click", () => {
+    input.value += i;
+  });
+}
+
+document.getElementById("plus").onclick = () => {
+  input.value += "+";
+};
+
+document.getElementById("minus").onclick = () => {
+  input.value += "-";
+};
+
+document.getElementById("multiply").onclick = () => {
+  input.value += "*";
+};
+
+document.getElementById("divide").onclick = () => {
+  input.value += "/";
+};
+
+document.getElementById("dot").onclick = () => {
+  input.value += ".";
+};
+
+document.getElementById("open").onclick = () => {
+  input.value += "(";
+};
+
+document.getElementById("close").onclick = () => {
+  input.value += ")";
+};
+
+document.getElementById("clr").onclick = () => {
+  input.value = "";
+};
+
+document.getElementById("back").onclick = () => {
+  input.value = input.value.slice(0, -1);
+};
+
+document.getElementById("ans").onclick = () => {
+  try {
+    input.value = eval(input.value);
+  } catch {
+    input.value = "Error";
+  }
+};
